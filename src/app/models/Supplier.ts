98acd3582d77999164436203f83e3233ee103eca
Santy0608 <barquerotorress@gmail.com>
2026-01-11 02:11:44 -0600

    Developing Supplier Class

---
+++ src/app/models/Supplier.ts
@@ -0,0 +1,13 @@
+export class Supplier{
+    idSupplier!:number;
+    name?:string;
+    rut?:string;
+    contact?:string;
+    phone?:string;
+    email?:string;
+    address?:string;
+    paymentTerms?:string;
+    averageDeliveryTime?:string;
+    agreedDiscount?:number;
+    notes?:string;
+}
