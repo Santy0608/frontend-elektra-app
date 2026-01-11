dcaefd1c89cdb3994c2c1a15263016f0e520672f
Santy0608 <barquerotorress@gmail.com>
2026-01-11 00:36:25 -0600

    Developing Part with it´s attributes

---
+++ src/app/models/Part.ts
@@ -0,0 +1,17 @@
+import { Category } from "./Category";
+
+export class Part{
+    idPart!: number;
+    name!:string;
+    code!:string;
+    brand!:string;
+    compatibleModel!:string;
+    price!:number;
+    stock!:number;
+    status!:string;
+    minimumStock!:number;
+    category!:Category;
+    
+
+    categoryId?: number;
+}
