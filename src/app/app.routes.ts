462375f6c34e45dd2b39c899f0477b3f915ca52c
Santy0608 <barquerotorress@gmail.com>
2026-01-12 00:57:36 -0600

    Updating routes for supplier module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { CategoryComponent } from './components/category/category.component';
 import { CategoryFormComponent } from './components/category-form/category-form.component';
 import { PartComponent } from './components/part/part.component';
 import { PartFormComponent } from './components/part-form/part-form.component';
+import { SupplierComponent } from './components/supplier/supplier.component';
+import { Supplier } from './models/Supplier';
 
 export const routes: Routes = [
 
@@ -17,8 +19,10 @@ export const routes: Routes = [
 
     { path: 'parts', component: PartComponent },
     { path: 'parts/create', component: PartFormComponent},
-    { path: 'parts/edit/:idPart', component: PartComponent}
+    { path: 'parts/edit/:idPart', component: PartComponent},
 
     //Routes for Suppliers
-
+    { path: 'suppliers', component: SupplierComponent},
+    { path: 'suppliers/create', component: SupplierComponent},
+    { path: 'suppliers/edit/:idPart', component: SupplierComponent}
 ];
