edaa296caff8294ecb61f9ba1bd135dc12601745
Santy0608 <barquerotorress@gmail.com>
2026-01-11 18:41:09 -0600

    Updating Routes in the App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CategoryComponent } from './components/category/category.component';
 import { CategoryFormComponent } from './components/category-form/category-form.component';
 import { PartComponent } from './components/part/part.component';
+import { PartFormComponent } from './components/part-form/part-form.component';
 
 export const routes: Routes = [
 
@@ -12,6 +13,12 @@ export const routes: Routes = [
     { path: 'categories/create', component: CategoryFormComponent},
     { path: 'categories/edit/:idCategory', component: CategoryFormComponent},
 
+    //Routes for parts
+
     { path: 'parts', component: PartComponent },
+    { path: 'parts/create', component: PartFormComponent},
+    { path: 'parts/edit/:idPart', component: PartComponent}
+
+    //Routes for Suppliers
 
 ];
