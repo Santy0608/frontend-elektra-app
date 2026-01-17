59ccc88f2506cf287d1d6b540646e39a279aae70
Santy0608 <barquerotorress@gmail.com>
2026-01-16 22:17:32 -0600

    Updating App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CategoryComponent } from './components/category/category.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, RouterLink, CategoryComponent, CustomerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
