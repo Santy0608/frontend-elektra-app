83b99d834afcb2ff8deb4a423895ec481aa8e3d0
Santy0608 <barquerotorress@gmail.com>
2026-01-16 23:51:11 -0600

    Developing User Service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { User } from "../models/User";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService{
+
+    private url: string = 'http://localhost:8080/api/users';
+
+    constructor(private http: HttpClient){
+
+    }
+
+    userList(): Observable<User[]>{
+        return this.http.get<User[]>(this.url);
+    }
+
+    findUserById(id: number): Observable<User>{
+        return this.http.get<User>(`${this.url}/${id}`);
+    }
+
+    saveUser(user: User): Observable<User>{
+        return this.http.post<User>(this.url, user);
+    }
+
+    deleteUser(id: number): Observable<void>{
+        return this.http.delete<void>(`${this.url}/${id}`);
+    }
+
+}
